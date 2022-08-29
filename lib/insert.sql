28beee8bf965ec82a8a9eff3821e63dfaa1aaba1
bill apiyo <billian177@gmail.com>
2022-08-29 06:32:03 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 25, "M", "Brown", "aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 20, "F", "Red", "intense", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 18, "F", "Blue", "Nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sargeant Brown", 16, "M", "Green", "Slim", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 15, "M", "brown", "goofy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "Orange", "Grinchy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 20, "F", "Blue", "naive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 11, "M", "Yellow", "calm", 1);
